eacf91fb2cdaf5ebc3ebfc2740d5f1260186e5c9
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-04 13:30:26 -0300

    funcion ataque funciona, tira un alert

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -19,13 +19,13 @@ function Pokemon(nombre,color,vida,poderDeAtaque,nivelDeAmistad){
 		pokemon.vida = pokemon.vida - this.poderDeAtaque;
 	}
 }
-var Pikachu = new Pokemon("Pikachu","amarillo",120,100);
-var  Charmander = new Pokemon("Charmander","rojo",220,20);
+/*var Pikachu = new Pokemon("Pikachu","amarillo",120,100);
+var Charmander = new Pokemon("Charmander","rojo",220,20);
 var Squirtle = new Pokemon("Squirtle","azul",145,50);
 var Bulbasaur = new Pokemon("Bulbasaur","verde",115,30);
 
-var pokemon1 = document.getElementById('pokemon1');
-var pokemon2 = document.getElementById('pokemon2');
+var pokemon1 = document.getElementById('pokemon1').value;
+var pokemon2 = document.getElementById('pokemon2').value;
 
 var poke1 = pokemon1.options[pokemon1.selectedIndex].class;
 var poke2 = pokemon2.options[pokemon2.selectedIndex].class;
@@ -35,4 +35,20 @@ function ataque(poke1,poke2){
 }
 
 Pikachu.atacar(Charmander);
-console.log("Nivel de vida " + Charmander.vida);
+console.log("Nivel de vida " + Charmander.vida);*/
+
+function ataque(){
+	var pokemon1 = document.getElementById('pokemon1').value;
+	var pokemon2 = document.getElementById('pokemon2').value;
+
+	var pelea1 = new Pokemon(pokemon1,"verde",20,100);
+	var pelea2 = new Pokemon(pokemon2,"azul",30,220);
+
+	pelea1.atacar(pelea2);
+
+var ring = document.getElementsByClassName("styled-select");
+
+alert(pelea1.nombre + "Atacó a " + pelea2.nombre + "y su nivel de vida quedó en : " + pelea2.vida); 
+
+
+}
